4bde196b0dcf66462fc703ab89ac8e910eb088d1
Thomas Lau <thomas_0836@qq.com>
2020-04-26 11:25:09 +0800

    修改 base 配置 修复 在 github page上路由不一致的bug

--- .umirc.ts
+++ .umirc.ts
@@ -4,7 +4,9 @@ const isProduction = () => process.env.NODE_ENV === 'production';
 
 export default defineConfig({
   title: 'Library Name',
-  publicPath: isProduction() ? '/dumi_test/' : '/',
+  // publicPath: isProduction() ? '/dumi_test/' : '/',
+  base: '/dumi_test/',
+  publicPath: '/dumi_test/',
   hash: true,
   ignoreMomentLocale: true,
   // more config: https://d.umijs.org/config
