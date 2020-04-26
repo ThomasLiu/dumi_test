69f7e47374d3cf3d089c56b1b9c04daf05b98534
Thomas Lau <thomas_0836@qq.com>
2020-04-26 11:05:39 +0800

    添加 按照环境变量修改 publicPath 的配置

--- .umirc.ts
+++ .umirc.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'dumi';
 
+const isProduction = () => process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   title: 'Library Name',
-  base: '/dumi_test',
+  publicPath: isProduction() ? '/dumi_test' : '/',
+  hash: true,
+  ignoreMomentLocale: true,
   // more config: https://d.umijs.org/config
 });
